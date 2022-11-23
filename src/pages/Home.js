3b496e38f24b42184ced54fdd2a45b1851bdec07
Cormac Cureton <cormaccureton@gmail.com>
2022-11-23 13:02:42 +0100

    Adjust home page spacing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,15 @@ import playlistCover1 from "../assets/playlistCover1.png";
 import playlistCover2 from "../assets/playlistCover2.png";
 import weekndAlbum from "../assets/weekndAlbum.png";
 import dojaAlbum from "../assets/dojaAlbum.png";
+import bieberAlbum from "../assets/bieberAlbum.png";
+import keemAlbum from "../assets/keemAlbum.png";
 import { HorizCard, VertCard } from "../components";
 
 export function Home() {
   return (
     <MainPage>
-      <h2 className="text-white">Recently Played</h2>
-      <Row className="w-full">
+      <h2 className="text-white my-4">Recently Played</h2>
+      <Row className="w-full mb-8">
         <div className="flex flex-row overflow-scroll space-x-4">
           <VertCard
             title="Car jamz everyone knows"
@@ -45,8 +47,8 @@ export function Home() {
           />
         </div>
       </Row>
-      <h2 className="text-white">Recommended for you</h2>
-      <Row className="w-full">
+      <h2 className="text-white my-4">Recommended for you</h2>
+      <Row className="w-full mb-8">
         <div className="flex flex-row overflow-scroll space-x-4">
           <HorizCard
             type="Album"
@@ -71,6 +73,43 @@ export function Home() {
             release="2020"
             duration="14 songs, 56 min 17 sec"
             imgSrc={weekndAlbum}
+          />
+        </div>
+      </Row>
+      <h2 className="text-white my-4">Trending Now</h2>
+      <Row className="w-full mb-8">
+        <div className="flex flex-row overflow-scroll space-x-4">
+          <HorizCard
+            type="Song"
+            artist="Justin Bieber, Don Toliver"
+            title="Honest (feat. Don Toliver)"
+            release="2022"
+            duration="3 min 13 sec"
+            imgSrc={bieberAlbum}
+          />
+          <HorizCard
+            type="Song"
+            artist="Baby Keem"
+            title="HONEST"
+            release="2019"
+            duration="2 min 52 sec"
+            imgSrc={keemAlbum}
+          />
+          <HorizCard
+            type="Song"
+            artist="Justin Bieber, Don Toliver"
+            title="Honest (feat. Don Toliver)"
+            release="2022"
+            duration="3 min 13 sec"
+            imgSrc={bieberAlbum}
+          />
+          <HorizCard
+            type="Song"
+            artist="Baby Keem"
+            title="HONEST"
+            release="2019"
+            duration="2 min 52 sec"
+            imgSrc={keemAlbum}
           />
         </div>
       </Row>
