f6d4266ccc3464ab4a7c580eaa79813907ffaff6
Cormac Cureton <cormaccureton@gmail.com>
2022-12-02 19:41:39 +0100

    Add togglable concerts section

--- src/pages/ArtistPage.js
+++ src/pages/ArtistPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { MainPage } from "./Middleware";
 import { Row, Image, Button } from "react-bootstrap";
@@ -9,12 +9,27 @@ import { MusicList } from "../components/MusicList";
 
 const artists = Music.getArtists();
 
+const concerts = [
+  {
+    city: "London",
+    date: "12/12",
+    venue: "Wembley Stadium",
+    src: "london.jpg",
+  },
+  {
+    city: "Paris",
+    date: "12/22",
+    venue: "Stade de France",
+    src: "paris.jpeg",
+  },
+];
+
 export function ArtistPage() {
   let { id } = useParams();
   const artist = artists[id];
 
   const songs = Music.getSongs({ by: id });
-  console.log(songs, "songs");
+  const [showConcerts, setShowConcerts] = useState(false);
 
   return (
     <MainPage>
@@ -30,11 +45,34 @@ export function ArtistPage() {
           <Button className="!rounded-full !bg-slate-800 !border-purple-light !font-semibold mr-8">
             Follow
           </Button>
-          <Button className="!rounded-full !bg-slate-800 !border-purple-light !font-semibold mr-8">
+          <Button
+            className="!rounded-full !bg-slate-800 !border-purple-light !font-semibold mr-8"
+            onClick={() => setShowConcerts(!showConcerts)}
+          >
             Upcoming Concerts
           </Button>
         </div>
       </Row>
+      {showConcerts && (
+        <div>
+          <h2 className="text-white my-4">Upcoming Concerts</h2>
+          <Row className="w-full mb-8">
+            <div className="flex flex-row overflow-x-scroll space-x-4">
+              {concerts.map((concert) => {
+                return (
+                  <>
+                    <VertCard
+                      title={concert.city + "-" + concert.date}
+                      subtitle={concert.venue}
+                      imgSrc={require(`../assets/${concert.src}`)}
+                    />
+                  </>
+                );
+              })}
+            </div>
+          </Row>
+        </div>
+      )}
       <h2 className="text-white my-4">Top Songs</h2>
       <Row className="w-full mb-8">
         <MusicList songs={Object.keys(songs)}></MusicList>
